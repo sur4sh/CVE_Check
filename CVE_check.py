10e112aea3d3c4260ed2a09cbb754eb995e4a93a
Suresh-Anbazhagan-E3327 <59721219+Suresh-Anbazhagan-E3327@users.noreply.github.com>
2021-12-08 14:20:33 +0530

    Update CVE_check.py

--- CVE_check.py
+++ CVE_check.py
@@ -1,1 +1,19 @@
-
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas
+from datetime import date
+import twint
+import datetime
+from datetime import date
+from datetime import timedelta
+import pandas as pd
+import json
+import re
+import requests
+import mysql.connector
+from pathlib import Path
+import os
+today = date.today()
+def CVEnew(datas):
+    
